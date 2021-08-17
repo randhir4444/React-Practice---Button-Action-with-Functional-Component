7fc962026c7b242dcc7d67675d792d61c0eb8fd5
deadR00T <randhir4444@gmail.com>
2021-08-17 20:26:01 +0530

    firstcommit

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [change, setchange] = useState('');
+  function changedata(){
+    let c="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+    setchange(c);
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={changedata}>Click</button>
+      <p id="para">{change}</p>
     </div>
   );
 }
