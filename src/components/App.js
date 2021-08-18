f2acfc57cf5fdf0d600b1fb4d18700b0b9c2b49a
deadR00T <randhir4444@gmail.com>
2021-08-18 18:08:08 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,22 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [change, setchange] = useState('');
+  const [change, setchange] = useState(true);
   function changedata(){
-    let c="Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+    let c=change;
+    if(c){
+      c=false;
+    }
+    else{
+      c=true;
+    }
     setchange(c);
   }
   return (
     <div id="main">
       <button id="click" onClick={changedata}>Click</button>
-      <p id="para">{change}</p>
+      {change ? null : (
+				<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)}
     </div>
   );
 }
